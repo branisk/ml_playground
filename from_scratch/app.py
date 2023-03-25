f329a3e1b29560513f9a62c1b46356c70946048c
branisk <56201891+branisk@users.noreply.github.com>
2023-03-25 01:54:29 -0400

    Delete from_scratch directory

--- from_scratch/app.py
+++
@@ -1,6 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-dbc_css = "https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lux/bootstrap.min.css"
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE, dbc_css])
-app.title = 'ML from Scratch'
