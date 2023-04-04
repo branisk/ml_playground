e1106355f59c5002aee8bc459bdda41083a26ddd
Branislav Kesic <branisk@protonmail.com>
2023-04-03 21:46:02 -0400

    Added Regression Metrics

--- ml_playground/algorithms.py
+++ ml_playground/algorithms.py
@@ -230,22 +230,22 @@ class KMeans:
 #  Regression Metrics
 #  Return a value between 0 and 1, a larger value represents a greater fit
 def r2_score(Y_pred, Y_actual):
-    return 1 - (np.sum(Y_actual, -Y_pred)**2 / np.sum(Y_actual - Y_pred))
+    return 1 - (np.sum(Y_actual - Y_pred)**2 / np.sum(Y_actual - Y_pred))
 
 
 #  Mean Squared Error
 def mse(Y_pred, Y_actual):
     n = len(Y_pred)
-    return (np.sum(Y_actual, -Y_pred) ** 2) / n
+    return (np.sum(Y_actual - Y_pred) ** 2) / n
 
 
 #  Root Mean Squared Error (The Square root of MSE)
 def rmse(Y_pred, Y_actual):
     n = len(Y_pred)
-    return np.sqrt((np.sum(Y_actual, -Y_pred) ** 2) / n)
+    return np.sqrt((np.sum(Y_actual - Y_pred) ** 2) / n)
 
 
 #  Mean Absolute Error
 def mae(Y_pred, Y_actual):
     n = len(Y_pred)
-    return np.abs(np.sum(Y_actual, -Y_pred)) / n
+    return np.abs(np.sum(Y_actual - Y_pred)) / n
