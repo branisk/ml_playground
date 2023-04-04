aca4acf448615e3648ac72e535a843481af2c870
Branislav Kesic <branisk@protonmail.com>
2023-04-03 21:39:07 -0400

    Added Regression Metrics

--- ml_playground/algorithms.py
+++ ml_playground/algorithms.py
@@ -232,15 +232,18 @@ class KMeans:
 def r2_score(Y_pred, Y_actual):
     return 1 - (np.sum(Y_actual, -Y_pred)**2 / np.sum(Y_actual - Y_pred))
 
+
 #  Mean Squared Error
 def mse(Y_pred, Y_actual):
     n = len(Y_pred)
     return (np.sum(Y_actual, -Y_pred) ** 2) / n
 
+
 #  Root Mean Squared Error (The Square root of MSE)
 def rmse(Y_pred, Y_actual):
     n = len(Y_pred)
     return np.sqrt((np.sum(Y_actual, -Y_pred) ** 2) / n)
+
 
 #  Mean Absolute Error
 def mae(Y_pred, Y_actual):
